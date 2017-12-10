04ae18c18e182128c2e83220a7456a0c19b0889e
Dave <djtbdanner@yahoo.com>
2017-12-10 11:11:24 -0600

    add action generators

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,9 +2,8 @@ var redux = require('redux');
 
 console.log('Starting redux example!!');
 
-// requires return a state
-var nextHobbyId = 1;
-var nextMovieId = 1;
+//Name Reducer and action generators
+//-------------------------
 var nameReducer = (state = "Anonymous", action) => {
   switch (action.type) {
     case "CHANGE_NAME":
@@ -14,6 +13,20 @@ var nameReducer = (state = "Anonymous", action) => {
   };
 };
 
+// Action generator is a simple function that takes the items required to generate the object and returns the object with the name on it
+
+// Note - since the name of the variable is name of object identifier cna be single line - this is ES6
+var changeName = (name) => {
+  return{
+    type: "CHANGE_NAME",
+    //name: name
+    name
+  }
+};
+
+//Hobbies Reducer and action generators
+//-------------------------
+var nextHobbyId = 1;
 var hobbiesReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_HOBBY":
@@ -31,6 +44,24 @@ var hobbiesReducer = (state = [], action) => {
   };
 };
 
+var addHobby = (hobby) => {
+  return{
+    type: "ADD_HOBBY",
+    hobby
+  }
+};
+
+var removeHobby = (id) => {
+  return{
+    type: "REMOVE_HOBBY",
+    id
+  }
+};
+
+
+//Movies Reducer and action generators
+//-------------------------
+var nextMovieId = 1;
 var moviesReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_MOVIE":
@@ -48,6 +79,22 @@ var moviesReducer = (state = [], action) => {
       return state;
   };
 };
+
+var addMovie = (title, genre) => {
+  return{
+    type: "ADD_MOVIE",
+    title,
+    genre
+  }
+};
+
+var removeMovie = (id) => {
+  return{
+    type: "REMOVE_MOVIE",
+    id
+  }
+};
+
 
 var reducer = redux.combineReducers({name: nameReducer, hobbies: hobbiesReducer, movies: moviesReducer});
 
@@ -74,19 +121,32 @@ console.log('currentState', currentState);
 // };
 //store.dispatch(action);
 
-store.dispatch({type: 'CHANGE_NAME', name: 'Dave'});
-store.dispatch({type: 'CHANGE_NAME', name: 'Frank'});
-store.dispatch({type: 'ADD_MOVIE', title: 'Better Off Dead', genre: "Comedy"});
-store.dispatch({type: 'ADD_HOBBY', hobby: 'running'});
-store.dispatch({type: 'ADD_HOBBY', hobby: 'sleeping'});
-store.dispatch({type: 'ADD_HOBBY', hobby: 'eating'});
-store.dispatch({type: 'ADD_HOBBY', hobby: 'coding'});
-store.dispatch({type: 'ADD_MOVIE', title: 'Outlaw Jose Whales', genre: "Western"});
-store.dispatch({type: 'ADD_MOVIE', title: 'Batman', genre: "Action"});
-store.dispatch({type: 'CHANGE_NAME', name: 'Jill'});
-store.dispatch({type: 'REMOVE_HOBBY', id: 2});
-store.dispatch({type: 'REMOVE_HOBBY', id: 1});
-store.dispatch({type: 'REMOVE_MOVIE', id: 1});
+//store.dispatch({type: 'CHANGE_NAME', name: 'Dave'});
+store.dispatch(changeName('Dave'));
+//store.dispatch({type: 'CHANGE_NAME', name: 'Frank'});
+store.dispatch(changeName('Frank'));
+// store.dispatch({type: 'ADD_MOVIE', title: 'Better Off Dead', genre: "Comedy"});
+store.dispatch(addMovie('Better Off Dead', "Comedy"));
+// store.dispatch({type: 'ADD_HOBBY', hobby: 'running'});
+store.dispatch(addHobby('running'));
+// store.dispatch({type: 'ADD_HOBBY', hobby: 'sleeping'});
+store.dispatch(addHobby('sleeping'));
+// store.dispatch({type: 'ADD_HOBBY', hobby: 'eating'});
+store.dispatch(addHobby('eating'));
+// store.dispatch({type: 'ADD_HOBBY', hobby: 'coding'});
+store.dispatch(addHobby('coding'));
+// store.dispatch({type: 'ADD_MOVIE', title: 'Outlaw Jose Whales', genre: "Western"});
+store.dispatch(addMovie('Outlaw Jose Whales', "Western"));
+// store.dispatch({type: 'ADD_MOVIE', title: 'Batman', genre: "Action"});
+store.dispatch(addMovie('Spiderman', "Action"));
+// store.dispatch({type: 'CHANGE_NAME', name: 'Jill'});
+store.dispatch(changeName('Jill'));
+// store.dispatch({type: 'REMOVE_HOBBY', id: 2});
+store.dispatch(removeHobby(2));
+// store.dispatch({type: 'REMOVE_HOBBY', id: 1});
+store.dispatch(removeHobby(1));
+// store.dispatch({type: 'REMOVE_MOVIE', id: 1});
+store.dispatch(removeMovie(1));
 
 // /* pure function (no outside varibles,
 //    no change outside variables, always
