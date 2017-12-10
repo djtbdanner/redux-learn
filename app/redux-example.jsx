33f64cc56ab09fe52ef4937fc1c534c223e767a3
Dave <djtbdanner@yahoo.com>
2017-12-10 11:45:40 -0600

    add asynchronous location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,9 +1,10 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example!!');
 
 //Name Reducer and action generators
-//-------------------------
+// -------------------------
 var nameReducer = (state = "Anonymous", action) => {
   switch (action.type) {
     case "CHANGE_NAME":
@@ -17,7 +18,7 @@ var nameReducer = (state = "Anonymous", action) => {
 
 // Note - since the name of the variable is name of object identifier cna be single line - this is ES6
 var changeName = (name) => {
-  return{
+  return {
     type: "CHANGE_NAME",
     //name: name
     name
@@ -25,7 +26,7 @@ var changeName = (name) => {
 };
 
 //Hobbies Reducer and action generators
-//-------------------------
+// -------------------------
 var nextHobbyId = 1;
 var hobbiesReducer = (state = [], action) => {
   switch (action.type) {
@@ -45,22 +46,15 @@ var hobbiesReducer = (state = [], action) => {
 };
 
 var addHobby = (hobby) => {
-  return{
-    type: "ADD_HOBBY",
-    hobby
-  }
+  return {type: "ADD_HOBBY", hobby}
 };
 
 var removeHobby = (id) => {
-  return{
-    type: "REMOVE_HOBBY",
-    id
-  }
+  return {type: "REMOVE_HOBBY", id}
 };
 
-
 //Movies Reducer and action generators
-//-------------------------
+// -------------------------
 var nextMovieId = 1;
 var moviesReducer = (state = [], action) => {
   switch (action.type) {
@@ -81,22 +75,49 @@ var moviesReducer = (state = [], action) => {
 };
 
 var addMovie = (title, genre) => {
-  return{
-    type: "ADD_MOVIE",
-    title,
-    genre
-  }
+  return {type: "ADD_MOVIE", title, genre}
 };
 
 var removeMovie = (id) => {
-  return{
-    type: "REMOVE_MOVIE",
-    id
-  }
+  return {type: "REMOVE_MOVIE", id}
 };
 
+//Map Reducer and action generators
+// -------------------------
+var mapReducer = (state = {
+  isFetching: false,
+  url: undefined
+}, action) => {
+  switch (action.type) {
+    case "START_LOCATION_FETCH":
+      return {isFetching: true, url: undefined};
+    case "COMPLETE_LOCATION_FETCH":
+      return {isFetching: false, url: action.url};
+    default:
+      return state;
+  };
+};
 
-var reducer = redux.combineReducers({name: nameReducer, hobbies: hobbiesReducer, movies: moviesReducer});
+var startLocationFetch = () => {
+  return {type: "START_LOCATION_FETCH"}
+};
+
+var completeLocationFetch = (url) => {
+  return {type: "COMPLETE_LOCATION_FETCH", url}
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function(res) {
+    var loc = res.data.loc;
+    var baseUrl = "http://maps.google.com?q="
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+
+};
+
+var reducer = redux.combineReducers({name: nameReducer, hobbies: hobbiesReducer, movies: moviesReducer, map: mapReducer});
 
 // argument needs to be a pure function
 var store = redux.createStore(reducer, redux.compose(
@@ -107,14 +128,20 @@ var store = redux.createStore(reducer, redux.compose(
 //subscribe to changes - pass function
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = "Loading....";
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML="<a href='" + state.map.url + "' target='_blank'>View You Location</a>";
+  }
   console.log("new state", store.getState());
-  document.getElementById('app').innerHTML = state.name + state.hobbies;
+//  document.getElementById('app').innerHTML = state.name + state.hobbies;
 });
 //unsubscribe();
 
 var currentState = store.getState();
 console.log('currentState', currentState);
 
+fetchLocation();
 // var action = {
 //   type: 'CHANGE_NAME',
 //   name: 'Dave'
