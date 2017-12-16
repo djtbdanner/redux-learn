8dfcda9883b402079e1028e1d252e6fe534879fc
Dave <djtbdanner@yahoo.com>
2017-12-16 12:32:38 -0600

    split out files and

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,127 +3,9 @@ var axios = require('axios');
 
 console.log('Starting redux example!!');
 
-//Name Reducer and action generators
-// -------------------------
-var nameReducer = (state = "Anonymous", action) => {
-  switch (action.type) {
-    case "CHANGE_NAME":
-      return action.name;
-    default:
-      return state;
-  };
-};
-
-// Action generator is a simple function that takes the items required to generate the object and returns the object with the name on it
-
-// Note - since the name of the variable is name of object identifier cna be single line - this is ES6
-var changeName = (name) => {
-  return {
-    type: "CHANGE_NAME",
-    //name: name
-    name
-  }
-};
-
-//Hobbies Reducer and action generators
-// -------------------------
-var nextHobbyId = 1;
-var hobbiesReducer = (state = [], action) => {
-  switch (action.type) {
-    case "ADD_HOBBY":
-      return [
-        ...state, {
-          id: nextHobbyId++,
-          hobby: action.hobby
-        }
-      ];
-
-    case "REMOVE_HOBBY":
-      return state.filter((h) => h.id !== action.id);
-    default:
-      return state;
-  };
-};
-
-var addHobby = (hobby) => {
-  return {type: "ADD_HOBBY", hobby}
-};
-
-var removeHobby = (id) => {
-  return {type: "REMOVE_HOBBY", id}
-};
-
-//Movies Reducer and action generators
-// -------------------------
-var nextMovieId = 1;
-var moviesReducer = (state = [], action) => {
-  switch (action.type) {
-    case "ADD_MOVIE":
-      return [
-        ...state, {
-          id: nextMovieId++,
-          title: action.title,
-          genre: action.genre
-        }
-      ];
-
-    case "REMOVE_MOVIE":
-      return state.filter((m) => m.id !== action.id);
-    default:
-      return state;
-  };
-};
-
-var addMovie = (title, genre) => {
-  return {type: "ADD_MOVIE", title, genre}
-};
-
-var removeMovie = (id) => {
-  return {type: "REMOVE_MOVIE", id}
-};
-
-//Map Reducer and action generators
-// -------------------------
-var mapReducer = (state = {
-  isFetching: false,
-  url: undefined
-}, action) => {
-  switch (action.type) {
-    case "START_LOCATION_FETCH":
-      return {isFetching: true, url: undefined};
-    case "COMPLETE_LOCATION_FETCH":
-      return {isFetching: false, url: action.url};
-    default:
-      return state;
-  };
-};
-
-var startLocationFetch = () => {
-  return {type: "START_LOCATION_FETCH"}
-};
-
-var completeLocationFetch = (url) => {
-  return {type: "COMPLETE_LOCATION_FETCH", url}
-};
-
-var fetchLocation = () => {
-  store.dispatch(startLocationFetch());
-
-  axios.get('http://ipinfo.io').then(function(res) {
-    var loc = res.data.loc;
-    var baseUrl = "http://maps.google.com?q="
-    store.dispatch(completeLocationFetch(baseUrl + loc));
-  });
-
-};
-
-var reducer = redux.combineReducers({name: nameReducer, hobbies: hobbiesReducer, movies: moviesReducer, map: mapReducer});
-
-// argument needs to be a pure function
-var store = redux.createStore(reducer, redux.compose(
-  window.devToolsExtension
-  ? window.devToolsExtension()
-  : f => f));
+var actions = require('./actions/index');
+var store = require('./store/configureStore').configure();
+alert(actions);alert(store);
 
 //subscribe to changes - pass function
 var unsubscribe = store.subscribe(() => {
@@ -141,7 +23,7 @@ var unsubscribe = store.subscribe(() => {
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-fetchLocation();
+store.dispatch(actions.fetchLocation());
 // var action = {
 //   type: 'CHANGE_NAME',
 //   name: 'Dave'
@@ -149,31 +31,31 @@ fetchLocation();
 //store.dispatch(action);
 
 //store.dispatch({type: 'CHANGE_NAME', name: 'Dave'});
-store.dispatch(changeName('Dave'));
+store.dispatch(actions.changeName('Dave'));
 //store.dispatch({type: 'CHANGE_NAME', name: 'Frank'});
-store.dispatch(changeName('Frank'));
+store.dispatch(actions.changeName('Frank'));
 // store.dispatch({type: 'ADD_MOVIE', title: 'Better Off Dead', genre: "Comedy"});
-store.dispatch(addMovie('Better Off Dead', "Comedy"));
+store.dispatch(actions.addMovie('Better Off Dead', "Comedy"));
 // store.dispatch({type: 'ADD_HOBBY', hobby: 'running'});
-store.dispatch(addHobby('running'));
+store.dispatch(actions.addHobby('running'));
 // store.dispatch({type: 'ADD_HOBBY', hobby: 'sleeping'});
-store.dispatch(addHobby('sleeping'));
+store.dispatch(actions.addHobby('sleeping'));
 // store.dispatch({type: 'ADD_HOBBY', hobby: 'eating'});
-store.dispatch(addHobby('eating'));
+store.dispatch(actions.addHobby('eating'));
 // store.dispatch({type: 'ADD_HOBBY', hobby: 'coding'});
-store.dispatch(addHobby('coding'));
+store.dispatch(actions.addHobby('coding'));
 // store.dispatch({type: 'ADD_MOVIE', title: 'Outlaw Jose Whales', genre: "Western"});
-store.dispatch(addMovie('Outlaw Jose Whales', "Western"));
+store.dispatch(actions.addMovie('Outlaw Jose Whales', "Western"));
 // store.dispatch({type: 'ADD_MOVIE', title: 'Batman', genre: "Action"});
-store.dispatch(addMovie('Spiderman', "Action"));
+store.dispatch(actions.addMovie('Spiderman', "Action"));
 // store.dispatch({type: 'CHANGE_NAME', name: 'Jill'});
-store.dispatch(changeName('Jill'));
+store.dispatch(actions.changeName('Jill'));
 // store.dispatch({type: 'REMOVE_HOBBY', id: 2});
-store.dispatch(removeHobby(2));
+store.dispatch(actions.removeHobby(2));
 // store.dispatch({type: 'REMOVE_HOBBY', id: 1});
-store.dispatch(removeHobby(1));
+store.dispatch(actions.removeHobby(1));
 // store.dispatch({type: 'REMOVE_MOVIE', id: 1});
-store.dispatch(removeMovie(1));
+store.dispatch(actions.removeMovie(1));
 
 // /* pure function (no outside varibles,
 //    no change outside variables, always
